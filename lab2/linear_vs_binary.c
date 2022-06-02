87e39624d3062b355df4be3b27b14e9bc30f0616
chirumer <71926497+chirumer@users.noreply.github.com>
2022-06-02 10:39:25 +0530

    Update linear_vs_binary.c

--- lab2/linear_vs_binary.c
+++ lab2/linear_vs_binary.c
@@ -69,17 +69,21 @@ int main() {
         double time_taken;
     
         start = clock();
-        linear_search(elements, n, search_element);
+        for (int i = 0; i < 100000; i++) {
+            linear_search(elements, n, search_element);
+        }
         end = clock();
         time_taken = (end - start)/(double)CLOCKS_PER_SEC * 1000;
-        printf("Time taken for linear search with %d elements: %lf milliseconds\n", n, time_taken);
+        printf("Time taken for linear search with %d elements: %lf milliseconds (100k iterations)\n", n, time_taken);
     
     
         start = clock();
-        binary_search(elements, n, search_element);
+        for (int i = 0; i < 100000; i++) {
+            binary_search(elements, n, search_element);
+        }
         end = clock();
         time_taken = (end - start)/(double)CLOCKS_PER_SEC * 1000;
-        printf("Time taken for binary search with %d elements: %lf milliseconds\n", n, time_taken);
+        printf("Time taken for binary search with %d elements: %lf milliseconds (100k iterations)\n", n, time_taken);
     }
     
     return 0;
